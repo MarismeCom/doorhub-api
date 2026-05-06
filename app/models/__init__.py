7631bc91203bd2e30db7f732653dbeb4eecda0a0
Yongjie Xu <airsmon@gmail.com>
2026-05-06 10:27:52 +0800

    feature: attendances

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,6 @@
 from app.models.api_secret import ApiSecret
 from app.models.attendance import Attendance, AttendanceDaily, AttendanceRuleSetting, DoorLog, HolidayCalendar
-<<<<<<< HEAD
-=======
 from app.models.attendance_sync_setting import AttendanceSyncSetting
->>>>>>> main
 from app.models.device import Device
 from app.models.system_user import SystemUser
 from app.models.user import User
@@ -13,10 +10,7 @@ __all__ = [
     "Attendance",
     "AttendanceDaily",
     "AttendanceRuleSetting",
-<<<<<<< HEAD
-=======
     "AttendanceSyncSetting",
->>>>>>> main
     "Device",
     "DoorLog",
     "HolidayCalendar",
